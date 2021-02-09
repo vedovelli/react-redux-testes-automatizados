fc1b031fc74d61dc9515a549eac3d76038e06371
Fabio Vedovelli <vedovelli@gmail.com>
2021-02-09 18:29:12 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,7 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {
-      backgroundImage: () => ({
-        'hero-pattern': "url('/space.jpg')",
-      }),
-    },
+    extend: {},
   },
   variants: {
     extend: {},
